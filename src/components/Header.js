d50caa69ea9292791efa8423ef24eba4a2a5c5c4
Charles P. Oliver <cpoliver@gmail.com>
2017-05-20 13:59:58 +0200

    Copied existing components from previous tutorial `vintage-pokedex`

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = ({ text }) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{text}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+    elevation: 2,
+    height: 60,
+    justifyContent: 'center',
+    paddingTop: 15,
+    position: 'relative',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
+  }
+};
+
+export default Header;
