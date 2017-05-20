e512397ffeb5240b9fb2e5877f24f4d5abb3f11a
Charles P. Oliver <cpoliver@gmail.com>
2017-05-20 18:25:33 +0200

    Add logic for handling auth state change and logout

--- src/app.js
+++ src/app.js
@@ -3,19 +3,40 @@ import { View } from 'react-native';
 import firebase from 'firebase';
 
 import firebaseConfig from '../private/firebase';
-import { Header } from './components/common';
+import { Button, CardSection, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount() {
     firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ loggedIn: !!user });
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header text="Auth" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
