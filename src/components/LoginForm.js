e18b59e5dcda080285919060a355216aba33eef7
Charles P. Oliver <cpoliver@gmail.com>
2017-05-20 15:58:29 +0200

    Add basic auth logic to login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import firebase from 'firebase';
 
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: ''
   };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase.auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch(() => {
+              firebase.auth()
+                      .createUserWithEmailAndPassword(email, password)
+                      .catch(() => {
+                        this.setState({ error: 'Authentication failed' })
+                      });
+            })
+  }
 
   render() {
     return (
@@ -31,13 +49,25 @@ class LoginForm extends Component {
           />
         </CardSection>
 
-          <CardSection>
-            <Button>
-              Log In
-            </Button>
-          </CardSection>
-        </Card>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Log In
+          </Button>
+        </CardSection>
+      </Card>
     );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    alignSelf: 'center',
+    color: '#f00',
+    fontSize: 20
   }
 }
 
