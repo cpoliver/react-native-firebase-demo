8fa405468f43f6922ec86c9a8d2c362b715c9e13
Charles P. Oliver <cpoliver@gmail.com>
2017-05-20 14:24:50 +0200

    Add firebase lib

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
+
+import firebaseConfig from '../private/firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <View>
+        <Header text="Auth" />
         <Text>App</Text>
       </View>
     );
